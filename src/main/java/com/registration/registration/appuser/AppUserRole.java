9e6780eaae6bacffddd14fbba278c44043327d01
TigerSamurai <tigran.avetisyan97@gmail.com>
2024-05-20 22:26:28 +0400

    First Commit

---
+++ src/main/java/com/registration/registration/appuser/AppUserRole.java
@@ -0,0 +1,7 @@
+package com.registration.registration.appuser;
+
+public enum AppUserRole {
+
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/registration/registration/security/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.registration.registration.security.config;
+
+import com.registration.registration.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/v*/registration/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+
+}
